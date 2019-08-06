bd4daefc6603cead61669a4421e8dbf62454a6ad
afeiship <1290657123@qq.com>
2019-08-06 09:56:44 +0800

    add: hidden property

--- src/components/index.js
+++ src/components/index.js
@@ -18,24 +18,38 @@ export default class extends Component {
 
   static defaultProps = {
     value: '↺ 有新版本更新啦，点击刷新',
+    hidden: false,
     hasUpdate: false
   };
   /*===properties end===*/
 
-  _onClick = (_) => {
-    NxOfflineSw.update();
-  };
+  constructor(inProps) {
+    super(inProps);
+    this.state = {
+      hidden: inProps.hidden
+    };
+  }
 
   render() {
-    const { className, hasUpdate, value, ...props } = this.props;
+    const { className, hasUpdate, value, hidden, ...props } = this.props;
     return (
       hasUpdate && (
-        <div className={classNames(CLASS_NAME, className)} {...props}>
+        <div
+          className={classNames(CLASS_NAME, className)}
+          hidden={this.state.hidden}
+          {...props}>
           <a
             onClick={() => {
               NxOfflineSw.update();
             }}>
             <span className={`${CLASS_NAME}__text`}>{value}</span>
+          </a>
+          <a
+            className={`${CLASS_NAME}__close`}
+            onClick={() => {
+              this.setState({ hidden: true });
+            }}>
+            ㄨ
           </a>
         </div>
       )
